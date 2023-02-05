d4316a22bdd36142180d046ff6e3eaacc1551e71
Luis Felipe <luis.felipe32971@gmail.com>
2023-02-05 19:01:13 -0300

    commented consumer.py

--- consumer.py
+++ consumer.py
@@ -3,6 +3,16 @@ from confluent_kafka import Consumer
 
 def message_consumer(topics: list, consumer: dict):
 
+    """
+    This function acts as a consumer of a Kafka topic.
+
+    Arguments:
+        Topics: list.
+            The topics that will be read.
+        Consumer: dict.
+            The cluster configurations
+    """
+
     c = Consumer(consumer)
 
     c.subscribe(topics)
@@ -17,4 +27,3 @@ def message_consumer(topics: list, consumer: dict):
             continue
         data = msg.value()
         print(data)
-    c.close()
